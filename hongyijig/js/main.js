03def9166b2f95c740495052dc4f20ec45e6b9a6
Neeraj <nkneeraj99@gmail.com>
2019-07-01 10:15:52 +0530

    added new pages

--- hongyijig/js/main.js
+++ hongyijig/js/main.js
@@ -318,31 +318,31 @@ function checkPosition() {
 
 // $('.b-description_readmore_wrapper .js-description_readmore_wrapper\n').find('br').remove();
 
-jQuery(document).ready(function($) {
-    "use strict";
-    //  TESTIMONIALS CAROUSEL HOOK
-    $('#customers-testimonials').owlCarousel({
-        loop: true,
-        center: true,
-        items: 3,
-        margin: 0,
-        // autoplay: true,
-        dots:true,
-        // autoplayTimeout: 8500,
-        // smartSpeed: 450,
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            1170: {
-                items: 3
-            }
-        }
-    });
-});
+// jQuery(document).ready(function($) {
+//     "use strict";
+//     //  TESTIMONIALS CAROUSEL HOOK
+//     $('#customers-testimonials').owlCarousel({
+//         loop: true,
+//         center: true,
+//         items: 3,
+//         margin: 0,
+//         // autoplay: true,
+//         dots:true,
+//         // autoplayTimeout: 8500,
+//         // smartSpeed: 450,
+//         responsive: {
+//             0: {
+//                 items: 1
+//             },
+//             768: {
+//                 items: 2
+//             },
+//             1170: {
+//                 items: 3
+//             }
+//         }
+//     });
+// });
 
 
 // $(function () {
@@ -354,3 +354,44 @@ jQuery(document).ready(function($) {
 //     $('.slider-text').show();
 //
 // });
+
+
+//Dropdwon show hide//
+$(function() {
+    $('#locationselector').change(function(){
+        $('.location').hide();
+        $('#' + $(this).val()).show();
+    });
+});
+
+
+/* Light YouTube Embeds by @labnol */
+/* Web: http://labnol.org/?p=27941 */
+
+document.addEventListener("DOMContentLoaded",
+    function() {
+        var div, n,
+            v = document.getElementsByClassName("youtube-player");
+        for (n = 0; n < v.length; n++) {
+            div = document.createElement("div");
+            div.setAttribute("data-id", v[n].dataset.id);
+            div.innerHTML = labnolThumb(v[n].dataset.id);
+            div.onclick = labnolIframe;
+            v[n].appendChild(div);
+        }
+    });
+
+function labnolThumb(id) {
+    var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',
+        play = '<div class="play"></div>';
+    return thumb.replace("ID", id) + play;
+}
+
+function labnolIframe() {
+    var iframe = document.createElement("iframe");
+    var embed = "https://www.youtube.com/embed/ID?autoplay=1";
+    iframe.setAttribute("src", embed.replace("ID", this.dataset.id));
+    iframe.setAttribute("frameborder", "0");
+    iframe.setAttribute("allowfullscreen", "1");
+    this.parentNode.replaceChild(iframe, this);
+}
